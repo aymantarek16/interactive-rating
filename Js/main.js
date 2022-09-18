39d33c80ae0d7b5102b1e419a51cfd019a4aed63
aymantarek16 <aymantarek16@yahoo.com>
2022-09-18 15:22:19 +0200

    active disabled attribute on the submit button

--- Js/main.js
+++ Js/main.js
@@ -17,3 +17,14 @@ submitBtn.addEventListener("click", () => {
   document.querySelector(".rating").style.display = "none";
   document.querySelector(".thankYou").style.display = "flex";
 });
+
+// active disabled attribute on the submit button when the rating is not selected
+window.addEventListener("load", () => {
+  submitBtn.setAttribute("disabled", true);
+  // remove disabled attribute when the user select a rating
+  li.forEach((ele) => {
+    ele.addEventListener("click", () => {
+      submitBtn.removeAttribute("disabled");
+    });
+  });
+});
